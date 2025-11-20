7beb9c3bac69e08f4291781a1276cb2aecea8bf2
Gabriel Franco <gafranco.contato@gmail.com>
2025-11-20 09:07:23 -0300

    Add files via upload
    Apenas adicionei um arquivo .c e .h para facilitar para meus colegas que realizaram o desenvolvimento da parte da consulta de pontos

---
+++ Consulta.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#include "Consulta.h"
+
+void carregar_recorde(Jogador *recordista) {
+
+};
+
+void salvar_recorde(Jogador recordista) {
+
+};
--- Quiz.h
+++ Quiz.h
@@ -9,16 +9,10 @@ typedef struct {
     int resposta_correta;
 } Pergunta;
 
-typedef struct {
-    char nome[50];
-    int pontuacao;
-} Jogador;
 
 int carregar_perguntas(Pergunta quiz[], const char* nome_arquivo);
 void menu();
 void iniciar_quiz(Pergunta quiz[], int total_perguntas);
-void carregar_recorde(Jogador *recordista);
-void salvar_recorde(Jogador recordista);
 
 #endif
 
